23a47448c77ab017ead96961cb29607a1b469a57
David LeMieux <lemieuxster@gmail.com>
2011-07-27 09:50:11 -0700

    Added ID worker, config, and Logger. Need to work on Bit size. Issues with 64 -> 32 bit overflow

---
+++ config.js
@@ -0,0 +1,7 @@
+//NodeFlake config
+var config = {
+    dataCenterId : 0,
+    workerId : 1
+};
+
+module.exports = config;
--- nodeflake.js
+++ nodeflake.js
@@ -1,19 +1,37 @@
+//Load Config
+var config = require("./config.js");
+var LOG = require("./lib/utils/log.js");
+
+
+//Load dependencies
 var http = require('http'),
     redis = require('redis'),
-    redis_client = redis.createClient();
+    idworker = require("./lib/idworker.js");
 
+
+//Startup Info
+LOG.info('NodeFlake Server running at http://localhost:1337/');
+LOG.info('Data Center Id:' + config.dataCenterId);
+LOG.info('     Worker Id:' + config.workerId);
+
+
+//Local variables
+var redis_client = redis.createClient(),
+    worker = idworker.getIdWorker(config.workerId, config.dataCenterId, redis_client);
+
+//Listen for connections
 http.createServer(function (req, res) {
   redis_client.incr("nextid", function(err, value) {
       res.writeHead(200, {'Content-Type': 'application/json'});
       if (!err) {
-        var nextId = ((new Date).getTime()).toString(36) + "-" + (value).toString(36);
-        console.log(nextId);
+        var nextId = worker.getId(req.headers['user-agent']);
+        LOG.info(nextId);
         res.end("{\"id\":\"" + nextId + "\"}\n");
       } else {
+        LOG.error("Failed to return id");
         res.end("{\"err\":" + err + "}\n");
       }
 
   });
-}).listen(1337, "localhost");
+}).listen(1337);
 
-console.log('NodeFlake Server running at http://localhost:1337/');
