0f31c009cf7be54b95af67e1d1a8e67edc627765
David LeMieux <lemieuxster@gmail.com>
2011-07-30 16:06:36 -0700

    Adding sockets, package.json, and a test client (html)

--- nodeflake.js
+++ nodeflake.js
@@ -6,28 +6,27 @@ var LOG = require("./lib/utils/log.js");
 
 
 //Load dependencies
-var http = require('http'),
-    idworker = require("./lib/idworker.js");
-
+var idworker = require("./lib/idworker.js");
 
 //Startup Info
 LOG.info('NodeFlake Server running on port ' + config.port);
 LOG.info('Data Center Id:' + config.dataCenterId);
 LOG.info('     Worker Id:' + config.workerId);
 
-
 //Local variables
 var worker = idworker.getIdWorker(config.workerId, config.dataCenterId);
 
-//Listen for connections
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  try {
-    var nextId = worker.getId(req.headers['user-agent']);
-    res.end("{\"id\":\"" + nextId + "\"}\n");
-  } catch(err) {
-    LOG.error("Failed to return id");
-    res.end("{\"err\":" + err + "}\n");
-  }
-}).listen(config.port);
-
+//Listen for socket connections and respond
+var io = require("socket.io").listen(config.port);
+io.sockets.on('connection', function (socket) {
+    try {
+        //TODO can you get the UA from sockets?
+        var nextId = worker.getId("socket.io web socket");
+        socket.emit('response', {id:nextId})
+    } catch(err) {
+        LOG.error("Failed to return id");
+        socket.emit('response', {error:err});
+    } finally {
+        socket.disconnect();
+    }
+});
