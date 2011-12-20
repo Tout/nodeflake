60121e49a3f49a096eb6e539915ce1c8b564fc56
Kristine Robison <kris@tout.com>
2011-12-20 15:51:37 -0800

    Setting config to NOT use a tcp socket

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {
     port : parseFloat(process.env.PORT) || 1337,
     dataCenterId : '1',
     workerId : '1',
-    tcpSockets : 1,
+    tcpSockets : 0,
     sockFile : '/tmp/nodeflake.sock'
 };
 
