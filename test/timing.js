9fc2599e38f1bd43ed08f70519a6a9f58dacc239
Kristine Robison <kris@tout.com>
2011-12-20 15:54:00 -0800

    Added a simple check for duplicate ids

---
+++ test/timing.js
@@ -0,0 +1,18 @@
+var idworker = require("../lib/idworker.js");
+var worker = idworker.getIdWorker(0, 0);
+var hash = {};
+
+var startTimer = (new Date).getTime();
+
+for (var i = 0; i < 2000; ++i) {
+    var id = worker.getId("test agent");
+    if (hash[id]) {
+        console.log('**********************AUGH************************');
+    } else {
+        hash[id] = true;
+    }
+}
+
+var endTimer = (new Date).getTime();
+
+console.log(endTimer - startTimer, "milliseconds");
