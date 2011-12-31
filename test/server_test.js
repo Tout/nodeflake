a38e160ef082433e3a7f0a1852de2dae8f92b62c
Kristine Robison <kris@tout.com>
2011-12-30 22:55:12 -0800

    server level unit tests complete

---
+++ test/server_test.js
@@ -0,0 +1,28 @@
+var vows = require('vows'),
+    suite = vows.describe('server.js'),
+    server = require('./../lib/server.js'),
+    assert = require('assert'),
+    lastTimestamp = 0,
+    lastSequence = require('bigdecimal').BigInteger('0'),
+    testTimeSource = {'lastTimestamp': lastTimestamp,
+                      'lastSequence': lastSequence};
+
+var net = require('net'),
+    http = require('http');
+
+suite.addBatch({
+    'server': {
+        topic: server,
+        'returns a socket server when specified': function (topic) {
+            topic.start('socket', 8082, testTimeSource, function (server) {
+                assert.instanceOf(server, net.Server);
+            });
+        },
+        'returns an http server otherwise': function (topic) {
+            topic.start('http', 8083, testTimeSource, function (server) {
+                assert.instanceOf(server, http.Server);
+            });
+        }
+        
+    }
+}).export(module);
