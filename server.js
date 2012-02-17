18a857240641735f0eb7cebd6b1e15cebd51e2a6
Kristine Robison <kris@tout.com>
2012-02-17 11:12:44 -0800

    testing haibu deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+var haibu = require('haibu'),
+    eyes = require('eyes'),
+    app = require('./package.json');
+
+var client = new haibu.drone.Client({
+  host: 'localhost',
+  port: 9002
+});
+
+client.start(app, function (err, result) {
+  if (err) {
+    console.log('Error spawning app: ' + app.name);
+    return eyes.inspect(err);
+  }
+
+  console.log('Successfully spawned app:');
+  eyes.inspect(result);
+});
